5ca160c0436ac517320433b1cf144def2755aa6d
blank <hayaken3007@i.softbank.jp>
2023-12-01 12:03:27 +0900

    同一パッケージ内からの呼び出し

--- calculator/sum.go
+++ calculator/sum.go
@@ -1,8 +1,12 @@
 package calculator
 
+import "fmt"
+
 var offset float64 = 10
 var Offset float64 = 1
 
 func Sum(a float64, b float64) float64 {
+	// パッケージ内ならprivateでも呼び出せる
+	fmt.Println("multiply: ", multiply(a, b))
 	return a + b + offset
 }
