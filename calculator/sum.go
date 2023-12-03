2883ee6c7e02f45c5b6ba3d1fb0a671a9814c7cb
blank <hayaken3007@i.softbank.jp>
2023-12-04 03:55:03 +0900

    import方法の追記(非推奨)

--- calculator/sum.go
+++ calculator/sum.go
@@ -1,6 +1,10 @@
 package calculator
 
-import "fmt"
+import (
+	"fmt"
+	// . "fmt" // インポートの名前省略(非推奨)
+	// f "fmt" // インポートのエイリアス(非推奨)
+)
 
 var offset float64 = 10
 var Offset float64 = 1
@@ -8,5 +12,7 @@ var Offset float64 = 1
 func Sum(a float64, b float64) float64 {
 	// パッケージ内ならprivateでも呼び出せる
 	fmt.Println("multiply: ", multiply(a, b))
+	// f.Println("インポートのエイリアス(非推奨)")
+	// Println("インポートの名前省略(非推奨)")
 	return a + b + offset
 }
